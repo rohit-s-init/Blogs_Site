439fc238e9242bfcfbd6e9f64069f484467079df
rohit-s-init <rohitrameshwarsawant@gmail.com>
2026-04-05 00:09:45 +0530

    resolving cors error

--- server.js
+++ server.js
@@ -10,13 +10,29 @@ let app = express();
 const PORT = process.env.PORT || 3000;
 const __dirname = import.meta.dirname;
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://blogs-site-frontend.vercel.app",
+  "https://blogs-site-frontend-git-main-rohit-s-inits-projects.vercel.app"
+];
 
-app.use(cors(
-    {
-        origin: ["http://localhost:5173", "https://blogs-site-frontend.vercel.app"],
-        credentials: true
-    }
-));
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use(cookieParser());
 app.use(express.json());
